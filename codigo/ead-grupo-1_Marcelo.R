e39d2fe2458092f504869fafaf83beef8b03f7d2
marcelojsantos74 <marcelo.j.santos@unesp.br>
2023-01-07 08:48:13 -0300

    desenvolvendo grafico Perfil Genero

--- codigo/ead-grupo-1_Marcelo.R
+++ codigo/ead-grupo-1_Marcelo.R
@@ -12,10 +12,6 @@
 # Impacto da COVID-19 nos Estudantes Universitários no Brasil
 # Fonte de dados : 
 
-# Importacao arquivo CSV
-dataset.csv <-read.csv("./dados/COVID19IES.csv", header = TRUE, sep = ";", quote = "\"", dec = ".")
-
-
 # como os alunos vivenciaram a pandemia da COVID-19;
 # de que forma se comportaram frente as restrições impostas pelos riscos de contágio;
 # (quais suas considerações a respeito das estratégias que foram adotadas pelas instituições superiores; e
@@ -29,3 +25,32 @@ dataset.csv <-read.csv("./dados/COVID19IES.csv", header = TRUE, sep = ";", quote
 #       condicao financeira : renda, ajuda, endividamento, despesas   
 # 3. estrategias das insituicoes superiores
 # 4.     
+
+# Import packages
+library(tidyverse)
+library(ggplot2)
+
+# Importacao arquivo CSV
+dataset.csv <-read.csv("./dados/COVID19IES.csv", header = TRUE, sep = ";", quote = "\"", dec = ".")
+
+# Numero Total de Questionarios respondidos
+num_questionario = as.numeric(nrow(dataset.csv))
+print(paste("Numero de Questionarios Respondidos :", num_questionario))
+
+# Grafico Perfil : Genero
+genero <- table(dataset.csv$genero)
+df_genero <- as.data.frame(table(dataset.csv$genero))
+
+pie( main = "Genero",
+    genero,
+    edges = 200, 
+    radius = 0.8,
+    col= rainbow(4),
+    labels = genero)
+
+ggplot(dataset.csv, aes(genero, fill = genero))+
+geom_bar()
+scale_fill_manual("Genero")
+
+
+                        
