6ce2ada4ddfb4ff3056f597f6a53f7a2be371f41
marcelojsantos74 <marcelo.j.santos@unesp.br>
2023-01-06 22:08:10 -0300

    Inicio codigo Marcelo

--- codigo/ead-grupo-1_Marcelo.R
+++ codigo/ead-grupo-1_Marcelo.R
@@ -16,3 +16,16 @@
 dataset.csv <-read.csv("./dados/COVID19IES.csv", header = TRUE, sep = ";", quote = "\"", dec = ".")
 
 
+# como os alunos vivenciaram a pandemia da COVID-19;
+# de que forma se comportaram frente as restrições impostas pelos riscos de contágio;
+# (quais suas considerações a respeito das estratégias que foram adotadas pelas instituições superiores; e
+# como estes fatores influenciaram sua vida.
+
+# minha proposta
+# quantidade de respostas do questionario
+# 1.perfil : faixa etaria, genero, onde mora, cursos, nivel ensino
+# 2.as pessoas na pandemia : vacinacao, 
+#       psicologico : ansiedade, qualidade de vida, convivencia risco relevante
+#       condicao financeira : renda, ajuda, endividamento, despesas   
+# 3. estrategias das insituicoes superiores
+# 4.     
