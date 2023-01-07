0228df2ba0a085b6ee6feccaa18647dc7e5b8535
marcelojsantos74 <marcelo.j.santos@unesp.br>
2023-01-06 21:13:31 -0300

    Arquivo marcelo com importacao dos dados, analise dos dados e graficos

---
+++ codigo/ead-grupo-1_Marcelo.R
@@ -0,0 +1,18 @@
+# Universidade Estadual Paulista - UNESP
+# PPGMiT FAAC-UNESP
+#
+# Trabalho Final da Disciplina Ciência de Dados - 2022
+# Prof. Joao Pedro Albino
+#
+# Alunos :  Lívia Inglesis Barcellos
+#           Henrique Mercez
+#           Marcelo José dos Santos
+#           Shelley Navari
+#
+# Impacto da COVID-19 nos Estudantes Universitários no Brasil
+# Fonte de dados : 
+
+# Importacao arquivo CSV
+dataset.csv <-read.csv("./dados/COVID19IES.csv", header = TRUE, sep = ";", quote = "\"", dec = ".")
+
+
